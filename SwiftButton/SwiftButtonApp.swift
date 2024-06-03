db7dc448c18cec187c14a8d103cba1a18fd954e2
mTayo <m.adinlewa@gmail.com>
2024-06-03 15:13:48 +0100

    Initial Commit

---
+++ SwiftButton/SwiftButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftButtonApp.swift
+//  SwiftButton
+//
+//  Created by Adinlewa Olutayo on 03/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
